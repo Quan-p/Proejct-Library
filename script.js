94bbe279477b3d8ca1fb892f617a2db930f65bf7
Quan <quanp155@gmail.com>
2021-01-26 17:26:40 -0600

    Created array for book and constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Books will be stored into a simple array
+Add function that can take user input and store the new book objects into an array
+*/
+
+let myLibrary = [];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
