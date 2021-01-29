a4c9bc3ea9e7637d937e884be361c373f2003e7f
Quan <quanp155@gmail.com>
2021-01-28 19:08:40 -0600

    Added modal form and button, still some bugs to resolve

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 /*
 Books will be stored into a simple array
 Add function that can take user input and store the new book objects into an array
+Write function that loops through array and displays each book on the page
 */
 
 let myLibrary = [];
 
 function Book() {
 
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 function addBookToLibrary() {
